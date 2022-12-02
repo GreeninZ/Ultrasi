63f3304af63076118052596d4e6713f84cb4970e
Krystian2004-cyber <krystian.kisielow@gmail.com>
2022-12-02 20:26:16 +0100

    hierarchia_plikow
    dodanie hierarchi plikow i sceny gownej

---
+++ gra hackathon/Assets/skrypty/przenies_scene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class przenies_scene : MonoBehaviour
+
+{
+    //public GameObject Button;
+
+    public void przenies_do_gry()
+    {
+        SceneManager.LoadScene("level");
+    }
+
+}
